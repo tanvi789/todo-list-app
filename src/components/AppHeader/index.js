72344a17f5f2f3ba412db52624bc77d6a6ef7e74
Tanvi Nanda <tanvi789@gmail.com>
2021-07-07 14:37:29 +0100

    removed source

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -12,9 +12,9 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
             <div className='app-title-text'>
-              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <h1 className='app-title'>TO DO LIST</h1>
               <p className='app-intro'>
-                Using FaunaDB & Netlify functions
+                To Do Task list
               </p>
             </div>
           </div>
@@ -26,15 +26,15 @@ const AppHeader = (props) => {
             href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>
             <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
           </a>
-          <div className='view-src'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://github.com/netlify/netlify-faunadb-example'>
-              <img className='github-icon' src={github} alt='view repo on github' />
-              View the source Luke
-            </a>
-          </div>
+          {/*<div className='view-src'>*/}
+          {/*  <a*/}
+          {/*    target='_blank'*/}
+          {/*    rel='noopener noreferrer'*/}
+          {/*    href='https://github.com/netlify/netlify-faunadb-example'>*/}
+          {/*    <img className='github-icon' src={github} alt='view repo on github' />*/}
+          {/*    View the source Luke*/}
+          {/*  </a>*/}
+          {/*</div>*/}
         </div>
       </div>
     </header>

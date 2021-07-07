b1930fa4ea39b456840063fb7fea78a52c70e726
Tanvi Nanda <tanvi789@gmail.com>
2021-07-07 14:57:00 +0100

    updated App Header

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -10,7 +10,7 @@ const AppHeader = (props) => {
       <div className='app-title-wrapper'>
         <div className='app-title-wrapper'>
           <div className='app-left-nav'>
-            <img src={logo} className='app-logo' alt='logo' />
+            {/*<img src={logo} className='app-logo' alt='logo' />*/}
             <div className='app-title-text'>
               <h1 className='app-title'>TO DO TASKS</h1>
               <p className='app-intro'>
@@ -19,23 +19,23 @@ const AppHeader = (props) => {
             </div>
           </div>
         </div>
-        <div className='deploy-button-wrapper'>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>
-            <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
-          </a>
-          {/*<div className='view-src'>*/}
-          {/*  <a*/}
-          {/*    target='_blank'*/}
-          {/*    rel='noopener noreferrer'*/}
-          {/*    href='https://github.com/netlify/netlify-faunadb-example'>*/}
-          {/*    <img className='github-icon' src={github} alt='view repo on github' />*/}
-          {/*    View the source Luke*/}
-          {/*  </a>*/}
-          {/*</div>*/}
-        </div>
+        {/*<div className='deploy-button-wrapper'>*/}
+        {/*  <a*/}
+        {/*    target='_blank'*/}
+        {/*    rel='noopener noreferrer'*/}
+        {/*    href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>*/}
+        {/*    <img src={deployButton} className='deploy-button' alt='deploy to netlify' />*/}
+        {/*  </a>*/}
+        {/*  /!*<div className='view-src'>*!/*/}
+        {/*  /!*  <a*!/*/}
+        {/*  /!*    target='_blank'*!/*/}
+        {/*  /!*    rel='noopener noreferrer'*!/*/}
+        {/*  /!*    href='https://github.com/netlify/netlify-faunadb-example'>*!/*/}
+        {/*  /!*    <img className='github-icon' src={github} alt='view repo on github' />*!/*/}
+        {/*  /!*    View the source Luke*!/*/}
+        {/*  /!*  </a>*!/*/}
+        {/*  /!*</div>*!/*/}
+        {/*</div>*/}
       </div>
     </header>
   )
